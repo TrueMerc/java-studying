069d11aa69477983f095541e453e060fb179d6ba
TrueMerc <true_merc@mail.ru>
2018-11-25 13:31:34 +0300

    Remove issues from the last lecturer review.

--- reflection/src/main/java/ru/ryabtsev/se/TestExecutor.java
+++ reflection/src/main/java/ru/ryabtsev/se/TestExecutor.java
@@ -40,6 +40,7 @@ public class TestExecutor {
 
         /**
          * Constructor.
+         *
          * @param value - integer value which corresponding method type.
          */
         TestSuiteMethodType(final int value) {
@@ -56,91 +57,89 @@ public class TestExecutor {
 
     /**
      * Executes test suite.
+     *
      * @param testSuiteName test suite class name.
      * @return Test suite execution result.
      * @throws ClassNotFoundException if class with specified name doesn't exist.
      */
-    public static TestSuiteExecutionResult start( String testSuiteName ) throws ClassNotFoundException {
-        Class testSuite = Class.forName( testSuiteName );
-        return start( testSuite );
+    public static TestSuiteExecutionResult start(String testSuiteName) throws ClassNotFoundException {
+        Class testSuite = Class.forName(testSuiteName);
+        return start(testSuite);
     }
 
     /**
      * Executes test suite.
+     *
      * @param testSuite test suite class.
      * @return Test suite execution result.
      */
-    public static TestSuiteExecutionResult start( Class testSuite ) throws TestSuiteException {
+    public static TestSuiteExecutionResult start(Class testSuite) throws TestSuiteException {
 
         final List<Method> executionList = new ArrayList<>();
         Method beforeSuiteMethod = null;
         Method afterSuiteMethod = null;
         Method[] methods = testSuite.getMethods();
 
-        for( Method method : methods ) {
-            switch( getMethodType( method ) ) {
+        for (Method method : methods) {
+            switch (getMethodType(method)) {
                 case BEFORE_SUITE_METHOD:
-                    if( beforeSuiteMethod == null) {
+                    if (beforeSuiteMethod == null) {
                         beforeSuiteMethod = method;
-                    }
-                    else {
-                        throw new TestSuiteException( "There should be only one @BeforeSuite annotated method." );
+                    } else {
+                        throw new TestSuiteException("There should be only one @BeforeSuite annotated method.");
                     }
                     break;
                 case AFTER_SUITE_METHOD:
-                    if( afterSuiteMethod == null) {
+                    if (afterSuiteMethod == null) {
                         afterSuiteMethod = method;
-                    }
-                    else {
-                        throw new TestSuiteException( "There should be only one @AfterSuite annotated method." );
+                    } else {
+                        throw new TestSuiteException("There should be only one @AfterSuite annotated method.");
                     }
                     break;
                 case TEST_METHOD:
-                {
-                    executionList.add( method );
-                }
-                break;
+                    executionList.add(method);
+                    break;
                 default:
                     break;
             }
         }
 
-        initialize( beforeSuiteMethod, testSuite );
-        sortByPriority( executionList );
+        initialize(beforeSuiteMethod, testSuite);
+        sortByPriority(executionList);
         int succeeded = 0;
-        for( Method testMethod : executionList ) {
-           succeeded += executeTest( testMethod, testSuite ) ? 1 : 0;
+        for (Method testMethod : executionList) {
+            succeeded += executeTest(testMethod, testSuite) ? 1 : 0;
         }
-        finalize( afterSuiteMethod, testSuite );
+        finalize(afterSuiteMethod, testSuite);
 
-        return new TestSuiteExecutionResult( succeeded, executionList.size() - succeeded );
+        return new TestSuiteExecutionResult(succeeded, executionList.size() - succeeded);
     }
 
     /**
      * Returns test suite method type.
      */
-    private static TestSuiteMethodType getMethodType( Method method ) throws AnnotationErrorException {
+    private static TestSuiteMethodType getMethodType(Method method) throws AnnotationErrorException {
 
-        boolean annotationChecks[] = { false, false, false };
+        boolean annotationChecks[] = {false, false, false};
 
-        if( method.getAnnotation( BeforeSuite.class) != null ) {
+        if (method.getAnnotation(BeforeSuite.class) != null) {
             annotationChecks[0] = true;
         }
 
-        if( method.getAnnotation( AfterSuite.class) != null ) {
+        if (method.getAnnotation(AfterSuite.class) != null) {
             annotationChecks[1] = true;
         }
 
-        if( method.getAnnotation( Test.class ) != null ) {
+        if (method.getAnnotation(Test.class) != null) {
             annotationChecks[2] = true;
         }
 
         int index = 3;
-        for( int i = 0, counter = 0; i < annotationChecks.length; ++i ) {
+        for (int i = 0, counter = 0; i < annotationChecks.length; ++i) {
             counter += annotationChecks[i] ? 1 : 0;
             index = annotationChecks[i] ? i : index;
-            if( counter > 1) {
-                throw new AnnotationErrorException( "Different annotation applied to one method." );
+            if (counter > 1) {
+                throw new AnnotationErrorException("Different annotation applied to one method.");
             }
         }
 
@@ -149,26 +148,16 @@ public class TestExecutor {
 
     /**
      * Initializes test suite.
-     * @param method initialization method.
+     *
+     * @param method    initialization method.
      * @param testSuite test suite class.
      * @throws InitializationException
      */
     private static void initialize(final Method method, final Class testSuite) throws InitializationException {
         try {
-            if (method == null) {
-                throw new InitializationException();
-            }
-            method.invoke( testSuite.getConstructor().newInstance() );
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-            throw new InitializationException();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-            throw new InitializationException();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-            throw new InitializationException();
-        } catch (InstantiationException e) {
+            if (method == null) throw new InitializationException();
+            method.invoke(testSuite.getConstructor().newInstance());
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
             e.printStackTrace();
             throw new InitializationException();
         }
@@ -176,28 +165,19 @@ public class TestExecutor {
 
     /**
      * Executes test suite method.
+     *
      * @param testMethod - method which annotated as {@code @Test}
-     * @param testSuite - test suite class.
+     * @param testSuite  - test suite class.
      * @return Test result.
      */
     private static boolean executeTest(final Method testMethod, final Class testSuite) {
         try {
-            testMethod.invoke(  testSuite.getConstructor().newInstance() );
+            testMethod.invoke(testSuite.getConstructor().newInstance());
             return true;
         } catch (AssertionFailureException e) {
             e.getMessage();
             return false;
-        }
-        catch (IllegalAccessException e) {
-            e.printStackTrace();
-            return false;
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-            return false;
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-            return false;
-        } catch (InstantiationException e) {
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
             e.printStackTrace();
             return false;
         }
@@ -205,27 +185,18 @@ public class TestExecutor {
 
     /**
      * Finalizes test suite.
-     * @param method - finalization method.
+     *
+     * @param method    - finalization method.
      * @param testSuite - test suite class.
      * @throws FinalizationException
      */
     private static void finalize(final Method method, final Class testSuite) throws FinalizationException {
         try {
-            if (method == null) {
-                throw new FinalizationException();
-            }
-            method.invoke( testSuite.getConstructor().newInstance() );
-        } catch (IllegalAccessException e) {
+            if (method == null) throw new FinalizationException();
+            method.invoke(testSuite.getConstructor().newInstance());
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
             e.printStackTrace();
             throw new FinalizationException();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-            throw new FinalizationException();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-            throw new InitializationException();
         }
     }
 
@@ -244,7 +215,7 @@ public class TestExecutor {
     /**
      * Sorts methods which annotated as {@code @Test} by priority.
      */
-    private static void sortByPriority( List<Method> methods ) {
-        Collections.sort( methods, new PriorityDescendingComparator() );
+    private static void sortByPriority(List<Method> methods) {
+        Collections.sort(methods, new PriorityDescendingComparator());
     }
 }
