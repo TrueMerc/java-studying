fd07e2abe87b250ac05ba901beeb1af09de17c54
TrueMerc <true_merc@mail.ru>
2019-01-05 20:03:37 +0300

    Add new project for 'homework-4' branch. Add 'MainApplication', 'SingleLinkedList' и 'SingleLinkedListTest' classes.

---
+++ algorithms/list/src/main/java/ru/ryabtsev/collection/MainApplication.java
@@ -0,0 +1,12 @@
+package ru.ryabtsev.collection;
+
+/**
+ * Main application class.
+ */
+public class MainApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ algorithms/list/src/test/java/ru/ryabtsev/collection/SingleLinkedListTest.java
@@ -0,0 +1,17 @@
+package ru.ryabtsev.collection;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for SingleLinkedList class.
+ */
+public class SingleLinkedListTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
